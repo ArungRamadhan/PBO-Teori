cefd2b82034d32bdceefe736012eebd223454b5f
dindatry10 <65879490+dindatry10@users.noreply.github.com>
2020-05-30 18:14:44 +0700

    Delete ExFinal.java

--- ExFinal.java
+++
@@ -1,8 +0,0 @@
-public class ExFinal {
-    public static void main(String[] args) {
-        final int jumlah = 100;
-        jumlah =10 ;
-        //Error
-        //Karena nilai dari suatu variabel final tidak dapat diubah setelah sebelumnya ditetapkan
-    }
-}
